31d2be5ffb1a12b54ebbf06e015500fb7f46db5b
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-07-20 11:21:38 +0200

    Initial commit

---
+++ src/main/java/bw_team7/gestione_azienda_energia/GestioneAziendaEnergiaApplication.java
@@ -0,0 +1,13 @@
+package bw_team7.gestione_azienda_energia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestioneAziendaEnergiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestioneAziendaEnergiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bw_team7/gestione_azienda_energia/GestioneAziendaEnergiaApplicationTests.java
@@ -0,0 +1,13 @@
+package bw_team7.gestione_azienda_energia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestioneAziendaEnergiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
