aed905014e87a08c7fc8004e9597dcfa88b7edc7
Niccolò Sciucco <sciucco.niccolo@gmail.com>
2026-07-20 12:23:27 +0200

    added indirizzi

--- src/main/java/bw_team7/gestione_azienda_energia/province/repositories/ProvinciaRepository.java
+++ src/main/java/bw_team7/gestione_azienda_energia/province/repositories/ProvinciaRepository.java
@@ -4,8 +4,10 @@ import bw_team7.gestione_azienda_energia.province.entities.Provincia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProvinciaRepository extends JpaRepository<Provincia, UUID> {
+    Optional<Provincia> findByNome(String nome);
 }
