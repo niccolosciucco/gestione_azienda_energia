d1e4a06c4a05585b9b9c4e8efc6c9fab600d1418
VincenzoManfredi99 <vincenzomanfredi2050@gmail.com>
2026-07-20 20:19:17 +0200

    Cliente Controller

--- src/main/java/bw_team7/gestione_azienda_energia/clienti/controllers/ClienteController.java
+++ src/main/java/bw_team7/gestione_azienda_energia/clienti/controllers/ClienteController.java
@@ -1,8 +1,17 @@
 package bw_team7.gestione_azienda_energia.clienti.controllers;
 
+import bw_team7.gestione_azienda_energia.clienti.entities.Cliente;
+import bw_team7.gestione_azienda_energia.clienti.payloads.ClienteDTO;
 import bw_team7.gestione_azienda_energia.clienti.services.ClienteService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import bw_team7.gestione_azienda_energia.exceptions.custom.BadRequest;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/clienti")
@@ -13,4 +22,53 @@ public class ClienteController {
     public ClienteController(ClienteService clienteService) {
         this.clienteService = clienteService;
     }
+
+    //POST
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente saveCliente(@RequestBody @Validated ClienteDTO body, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            List<String> errorsList = validationResult.getFieldErrors().stream()
+                    .map(fieldError -> fieldError.getDefaultMessage())
+                    .toList();
+            throw new BadRequest("Errori di validazione: " + errorsList);
+        }
+        return this.clienteService.save(body);
+    }
+
+    //GET
+    @GetMapping
+    public Page<Cliente> getClienti(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(defaultValue = "id") String orderBy
+    ) {
+        return this.clienteService.getAll(page, size, orderBy);
+    }
+
+    //GET
+    @GetMapping("/{id}")
+    public Cliente getById(@PathVariable UUID id) {
+        return this.clienteService.findById(id);
+    }
+
+    //PUT
+    @PutMapping("/{id}")
+    public Cliente getByIdAndUpdate(@PathVariable UUID id, @RequestBody @Validated ClienteDTO body, BindingResult validationResult) {
+        if (validationResult.hasErrors()) {
+            List<String> errorsList = validationResult.getFieldErrors().stream()
+                    .map(fieldError -> fieldError.getDefaultMessage())
+                    .toList();
+            throw new BadRequest("Errori di validazione nella modifica: " + errorsList);
+        }
+        return this.clienteService.findByIdAndUpdate(id, body);
+    }
+
+    //DELETE
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void getByIdAndDelete(@PathVariable UUID id) {
+        this.clienteService.findByIdAndDelete(id);
+    }
+
 }
